b5f0ada9591a585835dba1abf51d10704f781bfb
Andrey <Andrey@26.83.27.182>
2023-07-09 18:30:06 +0300

    MCreator Merge commit 'd1396fe525bbc50b61e1c44bd50155a90aa01024' into 3639e07500030d25aa3e8981dcf87ec4040cd5d0

--- src/main/java/net/mcreator/dieselpunkmod/item/AncerineSyringeItem.java
+++ src/main/java/net/mcreator/dieselpunkmod/item/AncerineSyringeItem.java
@@ -1,18 +1,11 @@
 
 package net.mcreator.dieselpunkmod.item;
 
-import net.minecraft.world.level.Level;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.InteractionHand;
-
-import net.mcreator.dieselpunkmod.procedures.AncerineSyringeUseProcedure;
-import net.mcreator.dieselpunkmod.init.DieselpunkmodModTabs;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import javax.annotation.Nullable;
 
 public class AncerineSyringeItem extends Item {
+
 	public AncerineSyringeItem() {
 		super(new Item.Properties().tab(DieselpunkmodModTabs.TAB_ITEMS).stacksTo(16).rarity(Rarity.COMMON));
 	}
@@ -25,7 +18,8 @@ public class AncerineSyringeItem extends Item {
 		double y = entity.getY();
 		double z = entity.getZ();
 
-		AncerineSyringeUseProcedure.execute(world, x, y, z, entity, itemstack);
+		AncerineSyringeUseProcedure.execute();
 		return ar;
 	}
+
 }
